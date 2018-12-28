9551799cea17c6cbe614aa9c269bd6a01f3b017a
Chiranjit102 <46185274+Chiranjit102@users.noreply.github.com>
2018-12-28 12:36:30 +0530

    Update createpage.java
    IP Chnage

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.212.156.76:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://52.91.204.118:5432/postgres";
 			String username="postgres";
 			String password="";
 		
